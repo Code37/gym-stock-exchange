e76061b9ffb3067bfb6ab07a9485615e4643b723
Code37 <495673131@qq.com>
2019-12-07 17:36:15 -0500

    Update __init__.py
    No StockExchange

--- gym_exchange/__init__.py
+++ gym_exchange/__init__.py
@@ -2,7 +2,7 @@ from gym.envs.registration import register
 
 register(
     id='game-stock-exchange-v0',
-    entry_point='gym_exchange.envs:StockExchange',
+    entry_point='gym_exchange.envs:StockExchangeDiscrete',
 )
 
 register(
